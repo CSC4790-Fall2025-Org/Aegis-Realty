d088dcd9e7b5755e4e2cc59e9faf6a57a6504220
JayBradCoding <jbradl24@villanova.edu>
2025-09-17 11:31:04 -0400

    created database.py and config.py, modified main.py to start fastapi server, started work on firebase_utils.py

---
+++ backend/app/core/firebase_utils.py
@@ -0,0 +1,8 @@
+import firebase_admin
+from firebase_admin import credentials
+import os
+
+if os.path.exists("/etc/secrets/firebase-adminsdk.json"):
+    firebase_creds_path = "/etc/secrets/firebase-adminsdk.json"
+else:
+    firebase_creds_path = os.getenv("FIREBASE_CREDENTIALS")
