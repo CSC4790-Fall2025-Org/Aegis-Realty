12c28de1e4d17a3e0e03c06de5ea07d11c32c57a
JayBradCoding <jbradl24@villanova.edu>
2025-09-26 11:30:18 -0400

    Updated ai investment analysis to properly import packages

--- backend/app/utils/ai_investment_analysis.py
+++ backend/app/utils/ai_investment_analysis.py
@@ -1,20 +1,20 @@
 from google import genai
 import json
-from prompts import generate_investment_prompt
+from ..core.prompts import generate_investment_prompt
 from google.genai import types
-from .config import settings
+from ..core.config import settings
 
-api_key = settings. GOOGLE_GENAI_KEY
+api_key = settings.GOOGLE_GENAI_KEY
 genai.api_key = api_key
 
-client = genai.Client(api_key=api_key,)
+client = genai.Client(api_key=api_key)
 
 def ai_investment_analysis(property_data, roi, cap_rate):
     prompt = generate_investment_prompt(property_data, roi, cap_rate)
     res = client.models.generate_content(
         model="gemini-2.5-flash",
         config=types.GenerateContentConfig(
-          system_instruction="You are a professional real estate investment analyst"
+            system_instruction="You are a professional real estate investment analyst"
         ),
         contents=prompt
     )
